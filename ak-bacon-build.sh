24020d778f53ee657fb30755de3c176149d31bd0
anarkia1976 <stefano.villa1976@gmail.com>
2014-12-31 18:52:52 +0100

    ak-bacon-build: compile with stock tool chain

--- ak-bacon-build.sh
+++ ak-bacon-build.sh
@@ -21,7 +21,7 @@ AK_VER="$BASE_AK_VER$VER"
 
 # Vars
 export LOCALVERSION=~`echo $AK_VER`
-export CROSS_COMPILE=${HOME}/android/AK-arm-eabi-4.9/bin/arm-eabi-
+export CROSS_COMPILE=${HOME}/android/AK-linaro/4.7.3-2013.04.20130415/bin/arm-linux-gnueabihf-
 export ARCH=arm
 export SUBARCH=arm
 export KBUILD_BUILD_USER=ak
